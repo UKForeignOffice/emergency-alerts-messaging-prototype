8a0cbb057d2e7b78503ab14046f1df804a281eb8
Jonny Wyatt <jonnywyatt2@gmail.com>
2021-01-18 10:15:16 +0000

    No need to prefix country name with 'subscribe'

--- app/store.js
+++ app/store.js
@@ -29,41 +29,6 @@ module.exports = ({phoneNumber, userMessage}) => {
   }
   const message = userMessage.trim().toLowerCase();
 
-  if (message === 'subscribe list') {
-    map[phoneNumber].lastTemplateSent = TEMPLATE_IDS.LIST_SUBSCRIBED_COUNTRIES;
-    return {...map[phoneNumber], countries: map[phoneNumber].countries.length ? map[phoneNumber].countries : 'none'};
-  }
-  const subscribeRequest = message.startsWith('subscribe ');
-  const unsubscribeRequest = message.startsWith('unsubscribe ');
-
-  if (subscribeRequest || unsubscribeRequest) {
-    const requestedCountryName = message.split(' ')[1];
-    const recognisedCountry = matchCountryName(requestedCountryName);
-    if (recognisedCountry) {
-      map[phoneNumber].lastCountryRequested = recognisedCountry;
-      if (subscribeRequest) {
-        if (map[phoneNumber].isBritishNational === true) {
-          if (!map[phoneNumber].countries.includes(recognisedCountry)) {
-            map[phoneNumber].countries.push(recognisedCountry);
-          }
-          map[phoneNumber].lastTemplateSent = TEMPLATE_IDS.CONFIRM_SUBSCRIBED;
-        } else if (map[phoneNumber].isBritishNational === null) {
-          map[phoneNumber].lastTemplateSent = TEMPLATE_IDS.CONFIRM_BRITISH_NATIONAL;
-        } else {
-          map[phoneNumber].lastTemplateSent = TEMPLATE_IDS.DENIED_NON_BRITISH_NATIONAL;
-        }
-        return {...map[phoneNumber]};
-      }
-      // unsubscribe
-      map[phoneNumber].countries = map[phoneNumber].countries.filter(country => country !== recognisedCountry);
-      map[phoneNumber].lastTemplateSent = TEMPLATE_IDS.CONFIRM_UNSUBSCRIBED;
-      return {...map[phoneNumber]};
-    }
-    map[phoneNumber].lastCountryRequested = requestedCountryName;
-    map[phoneNumber].lastTemplateSent = TEMPLATE_IDS.COUNTRY_NOT_RECOGNISED;
-    return {...map[phoneNumber]};
-  }
-
   if (message === 'yes' && map[phoneNumber].isBritishNational === null && map[phoneNumber].lastTemplateSent === TEMPLATE_IDS.CONFIRM_BRITISH_NATIONAL && map[phoneNumber].lastCountryRequested) {
     if (!map[phoneNumber].countries.includes(map[phoneNumber].lastCountryRequested)) {
       map[phoneNumber].countries.push(map[phoneNumber].lastCountryRequested);
@@ -78,6 +43,42 @@ module.exports = ({phoneNumber, userMessage}) => {
     map[phoneNumber].lastTemplateSent = TEMPLATE_IDS.DENIED_NON_BRITISH_NATIONAL;
     return {...map[phoneNumber]};
   }
+
+  // list all subscribed countries
+  if (message === 'subscribe list') {
+    map[phoneNumber].lastTemplateSent = TEMPLATE_IDS.LIST_SUBSCRIBED_COUNTRIES;
+    return {...map[phoneNumber], countries: map[phoneNumber].countries.length ? map[phoneNumber].countries : 'none'};
+  }
+
+  const unsubscribeRequest = message.startsWith('unsubscribe ');
+  const requestedCountryName = unsubscribeRequest ? message.split(' ')[1] : message;
+  const recognisedCountry = matchCountryName(requestedCountryName);
+  if (recognisedCountry) {
+    map[phoneNumber].lastCountryRequested = recognisedCountry;
+    if (unsubscribeRequest) {
+      // unsubscribe
+      map[phoneNumber].countries = map[phoneNumber].countries.filter(country => country !== recognisedCountry);
+      map[phoneNumber].lastTemplateSent = TEMPLATE_IDS.CONFIRM_UNSUBSCRIBED;
+      return {...map[phoneNumber]};
+    }
+    // subscribe
+    if (map[phoneNumber].isBritishNational === true) {
+      if (!map[phoneNumber].countries.includes(recognisedCountry)) {
+        map[phoneNumber].countries.push(recognisedCountry);
+      }
+      map[phoneNumber].lastTemplateSent = TEMPLATE_IDS.CONFIRM_SUBSCRIBED;
+    } else if (map[phoneNumber].isBritishNational === null) {
+      map[phoneNumber].lastTemplateSent = TEMPLATE_IDS.CONFIRM_BRITISH_NATIONAL;
+    } else {
+      map[phoneNumber].lastTemplateSent = TEMPLATE_IDS.DENIED_NON_BRITISH_NATIONAL;
+    }
+    return {...map[phoneNumber]};
+  }
+
+  // unrecognised country
+  map[phoneNumber].lastCountryRequested = requestedCountryName;
+  map[phoneNumber].lastTemplateSent = TEMPLATE_IDS.COUNTRY_NOT_RECOGNISED;
+  return {...map[phoneNumber]};
 
   map[phoneNumber].lastTemplateSent = null;
   return {...map[phoneNumber]};
