cc9a71309b57a3d49d35a14409a7c3c55efcaef7
Jonny Wyatt <jonnywyatt2@gmail.com>
2021-02-18 16:19:29 +0000

    Alert history - show email / SMS variations

--- app/data/alerts-myanmar.js
+++ app/data/alerts-myanmar.js
@@ -17,7 +17,9 @@ module.exports = {
     
   Due to intermittent internet services, ATMs may not be routinely available.
   `,
-      sms: `tbc`
+      sms: `There is an increased security presence across the country. You should remain cautious and avoid all demonstrations, large crowds and political gatherings. If you need to collect essential provisions, you should do so quickly and avoiding crowds. 
+    
+  Due to intermittent internet services, ATMs may not be routinely available.`
     },
     {
       date: '2021-02-02T13:10:00.000Z',
@@ -33,14 +35,23 @@ Please be aware that there may be disruptions to phone networks.
 
 If you’re in the UK and worried about a British national in Myanmar, call 020 7008 1500.
   `,
-      sms: `tbc`
+      sms: `If you’re in Myanmar and you need urgent help (for example, you’ve been attacked, arrested or someone has died), use one of the following numbers:
+
+* +95 (01) 370863
+* +95 (01) 370864
+* +95 (01) 370865
+* +95 (01) 370867
+
+Please be aware that there may be disruptions to phone networks.
+
+If you’re in the UK and worried about a British national in Myanmar, call 020 7008 1500.`
     },
     {
       date: '2021-02-04T17:00:00.000Z',
       title: 'Nationwide curfew',
       email: `There is a nationwide curfew imposed between midnight and 4am.
   `,
-      sms: `tbc`
+      sms: `There is a nationwide curfew imposed between midnight and 4am`
     },
     {
       date: '2021-02-04T18:55:00.000Z',
@@ -64,7 +75,16 @@ You will need to pay for your return travel to the UK. If departure options are 
 
 The Embassy cannot provide further information or advice about departure options over the phone. To get the latest information, [sign up for travel advice email alerts](/foreign-travel-advice/Myanmar/email-signup).
   `,
-      sms: `tbc`
+      sms: `There are limited options for British nationals looking to return to the UK.
+
+You should check with the embassies of respective transit countries to check transit, entry and Covid-19 testing requirements.
+
+* From 3 Feb Myanmar Airways International will fly from Yangon to Kuala Lumpur three times a week.
+
+* From 5 February Myanmar Airways International will operate a weekly service from Yangon to Seoul Incheon airport. A COVID-19 negative test result is required to join this flight.
+
+* From 5 February Singapore Airlines (SIA) will operate a weekly service to Singapore Changi airport. Transit is permitted for those joining flights to a range of destinations using SIA services. But this does not include the UK. No-one with a travel history to the UK in the preceding 14 days will be allowed to transit or enter Singapore. A COVID-19 negative test result is required to join this flight
+`
     },
     {
       date: '2021-02-09T08:12:00.000Z',
@@ -72,7 +92,7 @@ The Embassy cannot provide further information or advice about departure options
       email: `
     The military has shut down access to various internet platforms and reports of disruptions to wider internet, phone networks and ATMs are widespread.  You are advised to stay home and stay safe. If you need to collect essential provisions, you should do so quickly and avoiding crowds. There is a nationwide curfew imposed between midnight and 4am.
   `,
-      sms: `tbc`
+      sms: `The military has shut down access to various internet platforms and reports of disruptions to wider internet, phone networks and ATMs are widespread.  You are advised to stay home and stay safe. If you need to collect essential provisions, you should do so quickly and avoiding crowds. There is a nationwide curfew imposed between midnight and 4am.`
     },
     {
       date: '2021-02-11T14:58:00.000Z',
@@ -80,7 +100,7 @@ The Embassy cannot provide further information or advice about departure options
       email: `
    The nationwide curfew has been extended to between 8pm and 4am until further notice. 
   `,
-      sms: `tbc`
+      sms: `The nationwide curfew has been extended to between 8pm and 4am until further notice.`
     }
   ]
 };
