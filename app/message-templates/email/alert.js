f4dfb82a5d6336a31610d370363f58e99342d951
Jonny Wyatt <jonnywyatt2@gmail.com>
2021-03-09 15:22:24 +0000

    Email alert unsubscribe link

--- app/message-templates/email/alert.js
+++ app/message-templates/email/alert.js
@@ -1,4 +1,4 @@
-module.exports = ({ countryUrlSlug, messageHeading, body }) => `
+module.exports = ({ email, country, countryUrlSlug, messageHeading, body }) => `
 
 # ${messageHeading}
 
@@ -8,5 +8,5 @@ This emergency incident in Myanmar began on 1st Feb 2021. To view all alerts sen
 
 The British Embassy is following the situation carefully and will continue to update the Travel Advice - https://www.gov.uk/foreign-travel-advice/${countryUrlSlug} 
     
-^ To unsubscribe, reply to this email with Unsubscribe in the subject line
+^ To unsubscribe, go to ${process.env.SUBSCRIBE_APP_HOST}/unsubscribe-email?unsubscribeEmail=${email}&unsubscribeCountry=${encodeURIComponent(country)}
 `;
