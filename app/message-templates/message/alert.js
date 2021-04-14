6daed0aeb7b5a464919386dcca75aa0c663aa93d
Jonny Wyatt <jonnywyatt2@gmail.com>
2021-04-14 13:30:36 +0100

    Use hosts in email templates

--- app/message-templates/message/alert.js
+++ app/message-templates/message/alert.js
@@ -5,7 +5,7 @@ ${title}
 
 ${body}
 
-All alerts for ${country}: https://fcdo-sms-subscriber.herokuapp.com/alert-history?country=${country}&sort=desc
+All alerts for ${country}: ${process.env.SEND_ALERT_APP_HOST}/alert-history?country=${country}&sort=desc
 
 ^ To unsubscribe, go to ${process.env.SUBSCRIBE_APP_HOST}/unsubscribe}
 `;
